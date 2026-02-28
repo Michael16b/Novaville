330e19ad76e7c2b8cbe8148c5c39c52fe48ad92f
Michaël Besily <michael.besily@gmail.com>
2026-02-28 11:20:07 +0100

    Revert "fix url launcher"
    This reverts commit af73c63480401a43047e6cea158598f22196a55b.

--- frontend/lib/features/users/presentation/pages/credentials_share_page.dart
+++ frontend/lib/features/users/presentation/pages/credentials_share_page.dart
@@ -300,7 +300,11 @@ class _CredentialsSharePageState extends State<CredentialsSharePage> {
                             const url = 'https://novaville.fr';
                             final uri = Uri.parse(url);
                             if (await url_launcher.canLaunchUrl(uri)) {
-                              await url_launcher.launchUrl(uri);
+                              await url_launcher.launchUrl(
+                                uri,
+                                mode:
+                                    url_launcher.LaunchMode.externalApplication,
+                              );
                             } else {
                               CustomSnackBar.showError(
                                 context,
