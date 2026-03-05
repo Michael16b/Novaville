277f217297a384d7c880a239ad28548fbcb37af9
Charlene2002 <cjcarquefou@gmail.com>
2026-03-05 18:04:03 +0100

    fix: replace main.dart

---
+++ frontend/lib/features/useful_info/presentation/pages/useful_info_page_provider.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import 'package:frontend/features/useful_info/application/bloc/useful_info_bloc.dart';
+import 'package:frontend/features/useful_info/application/bloc/useful_info_event.dart';
+import 'package:frontend/features/useful_info/data/useful_info_repository.dart';
+
+import 'useful_info_page.dart';
+
+class UsefulInfoPageProvider extends StatelessWidget {
+  const UsefulInfoPageProvider({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) =>
+          UsefulInfoBloc(repository: context.read<UsefulInfoRepository>())
+            ..add(const UsefulInfoRequested()),
+      child: const UsefulInfoPage(),
+    );
+  }
+}
